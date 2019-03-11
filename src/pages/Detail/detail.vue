bfd2d360c3eb71e7c3ff308213463411e51eb092
jerryHZLiang <798895110@qq.com>
2019-03-11 12:08:02 +0800

    完善编辑页面

--- src/pages/Detail/detail.vue
+++ src/pages/Detail/detail.vue
@@ -1,11 +1,20 @@
 <template>
-  <div id="detail">
-    <section class="user-info">
-      <img :src="user.avatar" :alt="user.username" :title="user.username" class="avatar">
-      <h3>{{title}}</h3>
-      <p><router-link :to="`/user/${user.id}`">{{user.username}}</router-link> 发布于{{friendlyDate(createdAt)}}</p>
-    </section>
-    <section class="article" v-html="markdown"></section>
+  <div id="edit">
+    <h1>编辑文章</h1>
+    <h3>文章标题</h3>
+    <el-input v-model="title"></el-input>
+    <p class="msg">限30个字</p>
+    <h3>内容简介</h3>
+    <el-input type="textarea" v-model="description"  :autosize="{ minRows: 2, maxRows: 6}"></el-input>
+    <p class="msg">限30个字</p>
+    <h3>文章内容</h3>
+    <el-input type="textarea" v-model="content" :autosize="{ minRows: 4, maxRows: 30}"></el-input>
+    <p class="msg">限30个字</p>
+    <p>
+      <label>是否展示到首页</label>
+      <el-switch v-model="atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+    </p>
+    <el-button @click="onEdit">确定</el-button>
   </div>
 </template>
 
--- src/pages/Edit/edit.js
+++ src/pages/Edit/edit.js
@@ -1,8 +1,32 @@
+import blog from '@/api/blog'
+
 export default {
   name: 'Login',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      blogId: null,
+      title: '',
+      description: '',
+      content: '',
+      atIndex: false
+    }
+  },
+  create(){
+    this.blogId =  this.$route.params.blogId
+    blog.getDetail({blogId: this.blogId}).then(res=>{
+      this.title = res.data.title
+      this.description = res.data.description
+      this.content = res.data.content
+      this.atIndex = res.data.atIndex
+    })
+  },
+  methods: {
+    onEdit() {
+      blog.updateBlog({blogId: this.blogId},{ title: this.title, content: this.content, description: this.description, atIndex: this.atIndex})
+        .then(res => {
+          this.$message.success(res.msg)
+          this.$router.push({ path: `/detail/${res.data.id}`})
+        })
     }
   }
 }
